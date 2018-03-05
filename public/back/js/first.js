6b2e356be4f093db85b54f1a703804e41dfc24ac
wangpeiqing <407507104@qq.com>
2018-03-06 00:00:35 +0800

    提交first

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -1,5 +1,18 @@
 ;$(function () {
     $(".btn_add").on('click', function () {
-        $("#operation").model('show');
+        $("#operation").modal('show');
+
+    })
+
+    $("#addCategory").on('click',function () {
+        console.log($('form').serialize());
+        // $.ajax({
+        //     url:'/category/addTopCategory',
+        //     type:'post',
+        //     data: $('form').serialize(),
+        //     success:function (info) {
+        //         console.log(info);
+        //     }
+        // })
     })
 })
