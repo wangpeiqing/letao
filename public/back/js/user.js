955e1843ae348408413e54ca3da3edb9494f3192
wangpeiqing <407507104@qq.com>
2018-03-06 16:18:22 +0800

    用户页面完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,32 +1,32 @@
 ;$(function () {
-    var page=1;
-    var pagesize=5;
+    var currentPage = 1;
+    var pagesize = 5;
 
     //渲染数据
-    function getUsers() {
-        // var page = page || 1;
-        // var pagesize = pagesize || 5;
+    function getUsers(page,pagesize) {
+        var page = page || 1;
+        var pagesize = pagesize || 5;
         $.ajax({
-            url:'/user/queryUser',
-            type:'get',
-            data:{
-                page:page,
-                pageSize:pagesize
+            url: '/user/queryUser',
+            type: 'get',
+            data: {
+                page: page,
+                pageSize: pagesize
             },
-            success:function (result) {
+            success: function (result) {
                 console.log(result);
-                var tem = template('userinfo',result);
+                var tem = template('userinfo', result);
                 $("tbody").html(tem);
                 //渲染分页
                 $("#pagination").bootstrapPaginator({
-                    bootstrapMajorVersion:3,
-                    currentPage:result.page,
-                    totalPages:Math.ceil(result.total/result.size),
-                    numberOfPages:5,
-                    onPageClicked:function (event, originalEvent, type,p) {
-                        page=p;
+                    bootstrapMajorVersion: 3,
+                    currentPage: result.page,
+                    totalPages: Math.ceil(result.total / result.size),
+                    numberOfPages: 5,
+                    onPageClicked: function (event, originalEvent, type, p) {
+                        currentPage = p;
                         //页码点击时出发
-                        getUsers(page);
+                        getUsers(p,pagesize);
                     }
                 })
             }
@@ -34,4 +34,29 @@
     }
 
     getUsers();
+
+    //用户启用和停用
+    function updateUser(id,isDelete){
+        if (id&&(isDelete!=1&&isDelete!=0)) {
+            return;
+        }
+        $.ajax({
+            url: '/user/updateUser',
+            type: 'post',
+            data: {id: id, isDelete: isDelete},
+            success: function (info) {
+                console.log(info);
+                getUsers(currentPage,5);
+            }
+        })
+    }
+
+    $("table").on('click', '.enable', function () {
+        console.log($(this).data('id'));
+        updateUser($(this).data('id'),1);
+    })
+    $("table").on('click', '.disable', function () {
+        console.log($(this).data('id'));
+        updateUser($(this).data('id'),0);
+    })
 })
